ee5fe21c93d1c9dbdef74bf9c467665e155d5a72
azizbek747 <azizbek747@proton.me>
2025-04-18 12:24:44 +0500

    #3 - MovieListItem component style

--- src/components/MovieListItem.vue
+++ src/components/MovieListItem.vue
@@ -1,14 +1,88 @@
 <template>
-  <li>Yunus emro</li>
+  <!-- <li class="list-group-item d-flex justify-content-between" :class="{'like': like}" @click="onLike"  > -->
+  <li class="list-group-item d-flex justify-content-between" >
+    <span class="list-group-item-label">Omar</span>
+    <input type="text" class="list-group-item-input" value="811" />
+    <div class="d-flex justify-content-center align-items-center">
+      <button type="button" class="btn-cookie btn-sm">
+        <i class="fas fa-cookie"></i>
+      </button>
+      <button type="button" class="btn-trash btn-sm">
+        <i class="fas fa-trash"></i>
+      </button>
+        <i class="fas fa-star"></i>
+    </div>
+  </li>
     
 </template>
 
 <script>
   export default {
-    
+
+    // data() {
+    //   return {
+    //     like: false
+    //   }
+    // },
+    // methods: {
+    //   onLike() {
+    //     this.like = !this.like
+    //   }
+    // }
   }
 </script>
 
 <style scoped>
-
+  .list-group-item {
+    padding: 15px 20px;
+    border-bottom: 1px solid #3d5a80;
+  }
+  .list-group-item:last-child {
+    border-bottom: none;
+  }
+  .list-group-item span {
+    width: 550px;
+    font-size: 18px;
+    cursor: pointer;
+  }
+  .list-group-item input {
+    font-size: 18px;
+    text-align: center;
+    border: none;
+    outline: none;
+  }
+  .list-group-item button {
+    width: 35px;
+    height: 35px;
+    margin: 3px;
+    font-size: 17px;
+    border: none;
+    cursor: pointer;
+  }
+  .list-group-item .btn-cookie {
+    color: #e09f3e;
+  }
+  .list-group-item .btn-trash {
+    color: #e5383b;
+  }
+  .list-group-item .fa-star {
+    width: 35px;
+    height: 35px;
+    margin: 3px;
+    font-size: 17px;
+    color: #ffd700;
+    transition: .3s all ease;
+    text-align: center;
+    line-height: 35px;
+    transform: translateX(40px);
+    opacity: 0;
+  }
+  .list-group-item.like .fa-star {
+    transform: translateX(0);
+    opacity: 1;
+  }
+  .list-group-item.favourite .list-group-item-label,
+  .list-group-item.favourite .list-group-item-input {
+    color: #e09f3e;
+  }
 </style>
